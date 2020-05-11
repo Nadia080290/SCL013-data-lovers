5c0843b9bc405ea0ad510655e19ff7b39c15fa51
Nadia080290 <ngallardome@gmail.com>
2020-05-11 00:45:28 -0400

    repositorio en cero

--- src/data.js
+++ src/data.js
@@ -1,11 +1,34 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+const filtroPorOcupacion = (rol, POTTER) => POTTER.filter((e) => e[rol]);
+// FILTRO PARA ROLES
+
+const filtroPorCasas = (casa, POTTER) => {
+  const filtraCasa = POTTER.filter((e) => e.house === casa);
+  return filtraCasa;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+const filtroPorGenero = (genero, POTTER) => {
+  const filtraGenero = POTTER.filter((e) => e.gender === genero);
+  return filtraGenero;
+};
+
+const ordenarAscendente = (POTTER) => POTTER.sort((a, b) => {
+  if (a.name > b.name) {
+    return 1;
+  }
+  return -1;
+});
+
+const buscador = (data, buscame) => {
+  console.log(buscame);
+  return data.filter((obj) => obj.name.toLowerCase().startsWith(buscame));
+};
+
+export {filtroPorOcupacion,filtroPorCasas, filtroPorGenero, ordenarAscendente, buscador, filtroPorEspecie
 };
 
 
+const filtroPorEspecie= (especie, POTTER) => {
+  const filtraEspecie = POTTER.filter((e) => e.species === especie);
+  return filtraEspecie;
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,83 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './../data/potter/potter.js';
+import POTTER from './data/potter/potter.js';
+import {filtroPorOcupacion,filtroPorCasas, filtroPorGenero, ordenarAscendente, buscador,} from './data.js';
+
+const cajaImagenes = document.getElementById('arrPotter');
+const cajaOcupacion = document.querySelector('#ocupacion');
+const cajaCasas = document.querySelector('#casas');
+const cajaGenero = document.querySelector('#genero');
+const btnBienvenida = document.querySelector('#btn-bienvenida');
+const portada = document.querySelector('#vista-uno');
+const headerView = document.querySelector('#nav');
+const ordenando = document.querySelector('#ordenar');
+
+
+const generarTemplateString = (data) => {
+  let templateString = '';
+  data.forEach((obj) => {
+    templateString += ` 
+    <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img class="mi-imagen" src="${obj.image}"/>
+        <h2 class="fontNameCharacter">${obj.name}</h2>
+      </div>
+      <div class="flip-card-back">
+        <div class="box">
+        <h1>${obj.name}</h1>
+          <p><strong>Especie:</strong> ${obj.species}</p>
+          <p><strong>Género:</strong> ${obj.gender}</p>
+          <p><strong>Casa:</strong> ${obj.house}</p>
+          <p><strong>Ansestro:</strong> ${obj.ancestry}</p>
+          <p><strong>Varita:</strong> <img src="../src/image/wand.png" alt=""><br>
+          <strong>Madera:</strong> ${obj.wand.wood}<br>
+          <strong>Núcleo:</strong> ${obj.wand.core}<br>
+          <strong>Longitud:</strong>${obj.wand.length}</p>
+          <p><strong>Patronus:</strong> ${obj.patronus}</p>
+          <p><strong>Actor:</strong> ${obj.actor}</p>
+          <p><strong>Vivo</strong>: ${obj.alive}</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  });
+  return templateString;
+};
+
+cajaImagenes.innerHTML = generarTemplateString(POTTER);
+
+cajaOcupacion.addEventListener('click', (event) => {
+  const ocupacionCapturada = event.target.id;
+  cajaImagenes.innerHTML = generarTemplateString(filtroPorOcupacion(ocupacionCapturada, POTTER));
+});
+
+cajaCasas.addEventListener('click', (event) => {
+  const casaCapturada = event.target.id;
+  cajaImagenes.innerHTML = generarTemplateString(filtroPorCasas(casaCapturada, POTTER));
+});
+
+cajaGenero.addEventListener('click', (event) => {
+  const generoCapturada = event.target.id;
+  cajaImagenes.innerHTML = generarTemplateString(filtroPorGenero(generoCapturada, POTTER));
+});
+
+btnBienvenida.addEventListener('click', () => {
+  portada.classList.add('hide');
+  headerView.classList.remove('hide');
+  cajaImagenes.classList.remove('hide');
+});
+
+ordenando.addEventListener('click', (event) => {
+  if (event.target.id === 'a') {
+    cajaImagenes.innerHTML = generarTemplateString(ordenarAscendente(POTTER));
+  } else {
+    cajaImagenes.innerHTML = generarTemplateString(ordenarAscendente(POTTER).reverse());
+  }
+});
+
+// BUSCADOR
+const porNombre = document.getElementById('buscarData');
+porNombre.addEventListener('input', (event) => {
+  cajaImagenes.innerHTML = generarTemplateString(buscador(POTTER, event.target.value));
+})
