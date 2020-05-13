14a1820b3b20e50794994cafc128ea0bb164652d
Nadia080290 <ngallardome@gmail.com>
2020-05-13 01:05:43 -0400

    modificaciones en todo

--- src/data.js
+++ src/data.js
@@ -1,33 +1,65 @@
-const filtroPorOcupacion = (rol, POTTER) => POTTER.filter((e) => e[rol]);
+const reestablecerCajitaImagenes = () => {
+  const cajaImagenes = document.querySelector('#arrPotter');
+  const cajavaritas = document.querySelector('#insertWands');
+  cajavaritas.classList.add('hide');
+  cajaImagenes.classList.remove('hide');
+}
+const filtroPorOcupacion = (rol, POTTER) => {
+  reestablecerCajitaImagenes();
+  return POTTER.filter((e) => e[rol]);
+};
 // FILTRO PARA ROLES
 
 const filtroPorCasas = (casa, POTTER) => {
+  reestablecerCajitaImagenes();
   const filtraCasa = POTTER.filter((e) => e.house === casa);
   return filtraCasa;
 };
 
 const filtroPorGenero = (genero, POTTER) => {
+  reestablecerCajitaImagenes();
   const filtraGenero = POTTER.filter((e) => e.gender === genero);
   return filtraGenero;
 };
 
 const ordenarAscendente = (POTTER) => POTTER.sort((a, b) => {
+  reestablecerCajitaImagenes();
   if (a.name > b.name) {
     return 1;
   }
   return -1;
 });
 
+//BUSCADOR
 const buscador = (data, buscame) => {
+  reestablecerCajitaImagenes();
   console.log(buscame);
   return data.filter((obj) => obj.name.toLowerCase().startsWith(buscame));
 };
 
-export {filtroPorOcupacion,filtroPorCasas, filtroPorGenero, ordenarAscendente, buscador, filtroPorEspecie
+//Filtrar por Especies.
+export const filterEspecie = (especie, POTTER) => {
+  reestablecerCajitaImagenes();
+  const especieFiltrada = POTTER.filter((e) => e.species === especie);
+  console.log(especieFiltrada);
+  
+  return especieFiltrada;
+};
+
+export {filtroPorOcupacion,filtroPorCasas, filtroPorGenero, ordenarAscendente, buscador
+};
+
+export const filterWands = (array) => {
+  const arrayWands = [...array]; 
+  console.log(array);
+  const filtered = array.map((obj) => { // Data filtrada
+    return { name: obj.name, wand: obj.wand, image: obj.image}
+  });
+  console.log(filtered);
+  return filtered;
+  // obj.wand.wood !== '' || obj.wand.core !== '' || obj.wand.length !== '');
 };
 
 
-const filtroPorEspecie= (especie, POTTER) => {
-  const filtraEspecie = POTTER.filter((e) => e.species === especie);
-  return filtraEspecie;
-};
+
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 
 import data from './../data/potter/potter.js';
 import POTTER from './data/potter/potter.js';
-import {filtroPorOcupacion,filtroPorCasas, filtroPorGenero, ordenarAscendente, buscador,} from './data.js';
+import {filtroPorOcupacion,filtroPorCasas, filtroPorGenero, ordenarAscendente, buscador,filterEspecie, filterWands} from './data.js';
 
-const cajaImagenes = document.getElementById('arrPotter');
+const cajaImagenes = document.querySelector('#arrPotter');
 const cajaOcupacion = document.querySelector('#ocupacion');
 const cajaCasas = document.querySelector('#casas');
 const cajaGenero = document.querySelector('#genero');
@@ -11,8 +11,20 @@ const btnBienvenida = document.querySelector('#btn-bienvenida');
 const portada = document.querySelector('#vista-uno');
 const headerView = document.querySelector('#nav');
 const ordenando = document.querySelector('#ordenar');
+const menuHamburguesa = document.querySelector("#hab");
+const cajaEspecies = document.querySelector('#ordenarEspecie');
+const cajavaritas = document.querySelector('#insertWands');
+const opcionVarita = document.querySelector('#varitas');
+
+const alternLogoMenu = document.querySelector('.alternLogoMenu');
+
+// Función para volver al inicio haciendo click en logo alterno del Menú
+alternLogoMenu.addEventListener('click', () => {
+  window.location.reload();
+});
 
 
+//tarjetas personajes info completa.
 const generarTemplateString = (data) => {
   let templateString = '';
   data.forEach((obj) => {
@@ -66,6 +78,8 @@ btnBienvenida.addEventListener('click', () => {
   portada.classList.add('hide');
   headerView.classList.remove('hide');
   cajaImagenes.classList.remove('hide');
+  menuHamburguesa.classList.remove('hide');
+  
 });
 
 ordenando.addEventListener('click', (event) => {
@@ -80,4 +94,42 @@ ordenando.addEventListener('click', (event) => {
 const porNombre = document.getElementById('buscarData');
 porNombre.addEventListener('input', (event) => {
   cajaImagenes.innerHTML = generarTemplateString(buscador(POTTER, event.target.value));
-})
+});
+
+//Ordenar por especies
+cajaEspecies.addEventListener('click', (event) => {
+  const especieCapturada = event.target.id;
+  cajaImagenes.innerHTML = generarTemplateString(filterEspecie(especieCapturada, POTTER));
+});
+
+
+// Función que crea cards mostrando descripción de varitas
+const cajaVaritasHarry = (data) => {
+  // console.log(data);
+  let templatePotter = '';
+  data.forEach((obj) => {
+    templatePotter += `
+    <div class="wand-card">
+      <img src="${obj.image}" alt="Image" class="wand-card-photo"/>
+      <div class="wand-description">
+        <h1>${obj.name}</h1>
+        <li>Madera: ${obj.wand.wood}</li>
+        <li>Núcleo: ${obj.wand.core}</li>
+        <li>Largo: ${obj.wand.length}</li>
+        <br>
+        <li>Patronus: ${obj.patronus}</li>
+      </div>
+    </div> `;
+  });
+ return templatePotter;
+};
+
+opcionVarita.addEventListener('click', (event) => {
+  cajaImagenes.classList.add('hide');
+  cajavaritas.classList.remove('hide');
+  cajavaritas.innerHTML = cajaVaritasHarry(POTTER);
+});
+
+
+  
+  
